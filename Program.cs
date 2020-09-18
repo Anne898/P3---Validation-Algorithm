30b1a08d4507ed6a0ae1384fb9bf7ec2139966a3
Anne898 <45019588+Anne898@users.noreply.github.com>
2020-09-18 14:12:12 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Validation_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Cantidad;
+            int numero = 1;
+
+            Console.WriteLine("Escribe un numero");
+
+            Cantidad = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= Cantidad; i++)
+            {
+
+                System.Console.Write(numero + ", ");
+
+            }
+
+            System.Console.Write(Cantidad);
+
+
+
+
+
+
+        }
+    }
+}
